a2d1322332fe970982ca939708050e03db46964e
Lain-dono <lain.dono@gmail.com>
2020-04-23 17:31:35 +0300

    rm gles

--- examples/nvg_demo/src/main.rs
+++ examples/nvg_demo/src/main.rs
@@ -2,12 +2,10 @@
 
 mod blendish;
 mod canvas;
-mod run_gles;
 mod run_wgpu;
 
 mod time;
 
 fn main() {
-    //run_gles::main();
     run_wgpu::main();
 }
--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -2,8 +2,6 @@ mod commands;
 mod paint;
 mod pipeline;
 
-pub mod gles;
-
 pub use self::commands::{CallKind, CmdBuffer};
 pub use self::paint::{convert, FragUniforms, Paint};
 pub use self::pipeline::{Pipeline, Target};
